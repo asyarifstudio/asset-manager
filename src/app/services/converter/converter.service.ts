03291cb60a95257020c767c41eeb70ab6c2dc2c6
sahmad <syarif.ahmad@thalesgroup.com>
2022-06-16 18:52:26 +0800

    use other API for rate

--- src/app/services/converter/converter.service.ts
+++ src/app/services/converter/converter.service.ts
@@ -14,9 +14,22 @@ export class ConverterService {
   }
 
   async getRate(from:Currency, to:Currency):Promise<number>{
-    const url:string = `https://free.currconv.com/api/v7/convert?q=${from}_${to}&compact=ultra&apiKey=0863168d8aafc72848b8`
-    const rate:any = await firstValueFrom(this.http.get(url));
     
-    return rate[`${from}_${to}`];
+    const url:string = `https://api.exchangerate.host/latest?base=${from}&symbols=${to}`
+    const rate:any = await firstValueFrom(this.http.get(url));
+    // {
+    //   "motd":{
+    //     "msg":"If you or your company use this project or like what we doing, please consider backing us so we can continue maintaining and evolving this project.",
+    //     "url":"https://exchangerate.host/#/donate"
+    //   },
+    //   "success":true,
+    //   "base":"SGD",
+    //   "date":"2022-06-16",
+    //   "rates":
+    //   {
+    //     "IDR":10610.9028
+    //   }
+    // }
+    return rate['rates'][to];
   }
 }
