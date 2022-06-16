0c497ff3af4e0714bba1d8437b5cf764a3856f5a
sahmad <syarif.ahmad@thalesgroup.com>
2022-06-16 18:52:41 +0800

    show asset evolution in chart

--- src/app/components/main-chart/main-chart.component.ts
+++ src/app/components/main-chart/main-chart.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
-import { Asset } from 'src/app/model/asset.model';
+import { fromEventPattern } from 'rxjs';
+import { Asset, Currency } from 'src/app/model/asset.model';
+import { ConverterService } from 'src/app/services/converter/converter.service';
 import { AssetSummary } from 'src/app/services/util/asset-summary';
 import { UtilService } from 'src/app/services/util/util.service';
 
@@ -14,14 +16,18 @@ export class MainChartComponent implements OnInit {
   @Input() assets!: Asset[];
 
   assetSummary?: AssetSummary;
-  constructor(private util: UtilService) {
-  
+  SGD_TO_IDR:number = 0;
+  constructor(private util: UtilService, private converter:ConverterService) {
+    
   }
 
   ngOnInit(): void {
-    this.util.summarizeAsset(this.assets).then((value)=>{
-      this.assetSummary = value;
-    })
+    this.init();
+  }
+
+  async init(){
+    this.SGD_TO_IDR = await this.converter.getRate(Currency.SGD,Currency.IDR);
+    this.assetSummary = await this.util.summarizeAsset(this.assets)
   }
 
   get
@@ -31,13 +37,34 @@ export class MainChartComponent implements OnInit {
               {
                   data:this.assetSummary!.monthly.map((value)=>value.total),
                   label:'Total dalam IDR',
-                  type:'bar'
+                  type:'line'
                   
               }
           ],
           labels:this.assetSummary!.monthly.map((value)=>`${value.monthText} - ${value.year}` )
-
       };
+
+      //add dataset for each 
+      let datasets = result.datasets;
+
+      for(let asset of this.assets){
+
+        let data:number[] = this.assetSummary!.monthly.map((value)=>{
+          let amount = value.assetValue.get(asset.id!)!;
+          if(amount){
+            return asset.currency == Currency.SGD?amount.amount*this.SGD_TO_IDR:amount.amount;
+          }
+          else{
+            return 0;
+          }
+        })
+        datasets.push({
+          data:data,
+          label:asset.name,
+          type:'bar',
+        })
+      
+      }
 
       return result;
   }
