bb9fa0ea916aa0949e2a89ee8e3e8c99f9184745
sahmad <syarif.ahmad@thalesgroup.com>
2022-06-17 16:32:50 +0800

    fix summary computation on overall inc

--- src/app/services/util/util.service.ts
+++ src/app/services/util/util.service.ts
@@ -9,7 +9,7 @@ import { AssetMonthlySummary, AssetSummary, AssetValue } from './asset-summary';
 })
 export class UtilService {
 
-  constructor(private converter:ConverterService) { }
+  constructor(private converter: ConverterService) { }
 
   numberToMonth(month: number): string {
     let result: string = "";
@@ -30,91 +30,87 @@ export class UtilService {
     return result;
   }
 
-  async summarizeAsset(assets:Asset[]):Promise<AssetSummary>{
-    let result:AssetSummary = {
-      monthly:[]
+  async summarizeAsset(assets: Asset[]): Promise<AssetSummary> {
+    let result: AssetSummary = {
+      monthly: []
     }
 
-    const SGD_TO_IDR = await this.converter.getRate(Currency.SGD,Currency.IDR);
+    const SGD_TO_IDR = await this.converter.getRate(Currency.SGD, Currency.IDR);
 
     //loop asset
-    for(let asset of assets){
+    for (let asset of assets) {
 
       //if no entry, break;
-      if(asset.entries == undefined) break;
+      if (asset.entries == undefined) break;
 
-      for(let entry of asset.entries!){
-        let monthly = result.monthly.find((value)=>value.month == entry.month && value.year == entry.year);
+      for (let entry of asset.entries!) {
+        let monthly = result.monthly.find((value) => value.month == entry.month && value.year == entry.year);
 
-        if(monthly == undefined){
+        if (monthly == undefined) {
           monthly = {
-            year:entry.year,
-            month:entry.month,
-            monthText:this.numberToMonth(entry.month),
-            total:0,
-            assetValue:new Map<string,AssetValue>(),
-            monthlyInc:0,
-            monthlyIncPer:0,
-            overallInc:0,
-            overallIncPer:0,
-            yearToDateInc:0,
-            yearToDateIncPer:0
+            year: entry.year,
+            month: entry.month,
+            monthText: this.numberToMonth(entry.month),
+            total: 0,
+            assetValue: new Map<string, AssetValue>(),
+            monthlyInc: 0,
+            monthlyIncPer: 0,
+            overallInc: 0,
+            overallIncPer: 0,
+            yearToDateInc: 0,
+            yearToDateIncPer: 0
           }
 
           result.monthly.push(monthly);
         }
-        
-        monthly.assetValue.set(asset.id!,{id:entry.id! ,amount:entry.amount});
-        if(asset.currency == Currency.IDR){
+
+        monthly.assetValue.set(asset.id!, { id: entry.id!, amount: entry.amount });
+        if (asset.currency == Currency.IDR) {
 
           monthly.total += entry.amount;
         }
-        else{
-          monthly.total += (entry.amount*SGD_TO_IDR);
+        else {
+          monthly.total += (entry.amount * SGD_TO_IDR);
         }
 
       }
 
+    }
 
-      //after it ready, perform sorting
-      result.monthly = result.monthly.sort((a,b)=>{
-        if(a.year != b.year){
-          return b.year - a.year;
-        }
-        else{
-          return b.month - a.month;
-        }
-      })
-
-      //compute the increment
-      for(let i=0;i<result.monthly.length - 1 ;i++){
-        let current = result.monthly[i];
-        let previous = result.monthly[i+1];
-        current.monthlyInc = current.total - previous.total;
-        current.monthlyIncPer = (current.monthlyInc * 100)/previous.total
+    //after it ready, perform sorting
+    result.monthly = result.monthly.sort((a, b) => {
+      if (a.year != b.year) {
+        return b.year - a.year;
       }
-
-      //compute yearToDate Increment
-      //compute increment from beginning;
-      let begYear:number = 0;
-      let currentYear:number = 0;
-      let beginMonthly:AssetMonthlySummary = result.monthly[result.monthly.length-1];
-      for(let monthly of result.monthly){
-        if(currentYear!=monthly.year){
-          //get new year
-          currentYear = monthly.year;
-          begYear = result.monthly.find((value)=>value.year == currentYear && value.month == 0)?.total!;
-        }
-        monthly.yearToDateInc = monthly.total - begYear;
-        monthly.yearToDateIncPer = monthly.yearToDateInc*100/begYear;
-
-        monthly.overallInc = monthly.total - beginMonthly.total;
-        monthly.overallInc = monthly.overallInc*100/beginMonthly.total;
+      else {
+        return b.month - a.month;
       }
+    })
 
+    //compute the increment
+    for (let i = 0; i < result.monthly.length - 1; i++) {
+      let current = result.monthly[i];
+      let previous = result.monthly[i + 1];
+      current.monthlyInc = current.total - previous.total;
+      current.monthlyIncPer = (current.monthlyInc * 100) / previous.total
+    }
 
-      
+    //compute yearToDate Increment
+    //compute increment from beginning;
+    let begYear: number = 0;
+    let currentYear: number = 0;
+    let beginMonthly: AssetMonthlySummary = result.monthly[result.monthly.length - 1];
+    for (let monthly of result.monthly) {
+      if (currentYear != monthly.year) {
+        //get new year
+        currentYear = monthly.year;
+        begYear = result.monthly.find((value) => value.year == currentYear && value.month == 0)?.total!;
+      }
+      monthly.yearToDateInc = monthly.total - begYear;
+      monthly.yearToDateIncPer = monthly.yearToDateInc * 100 / begYear;
 
+      monthly.overallInc = monthly.total - beginMonthly.total;
+      monthly.overallIncPer = monthly.overallInc * 100 / beginMonthly.total;
     }
 
     return result;
