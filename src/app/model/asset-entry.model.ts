4bd54cdbef35a53b4198f3e130768c70b9518a51
sahmad <syarif.ahmad@thalesgroup.com>
2022-12-01 13:40:41 +0800

    delete everything

--- src/app/model/asset-entry.model.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from "./model";
-
-export interface AssetEntry extends Model {
-    amount:number;
-    year:number;
-    month:number;
-}
--- src/app/model/model.ts
+++
@@ -1,5 +0,0 @@
-export interface Model {
-    id?:string;
-    createdAt?:number;
-    updatedAt?:number;
-}
